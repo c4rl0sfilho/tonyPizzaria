b1abf743accfecf2cd05880e7dfacd7675e15a5f
CARLOS EDUARDO DA SILVA FILHO <23111777@CFP127ALUNO.LOCAL>
2023-12-13 17:06:26 -0300

    finalizado

---
+++ home.js
@@ -0,0 +1,37 @@
+'use strict'
+
+const urlCategorias = 'http://localhost:8080/categorias' 
+const urlProdutos = 'http://localhost:8080/produtos'
+ 
+var imgCatLocal = []
+
+async function pegarCategorias(){
+    const responseCat = await fetch(urlCategorias)
+    const dadosCat = await responseCat.json()
+    const arrayCategorias = dadosCat.categorias
+
+    arrayCategorias.forEach(categorias => {
+        imgCatLocal.push(categorias.img_categoria)        
+    });
+
+    return imgCatLocal
+}
+
+function criarCategorias(imgCatLocal){
+        const container_categorias = document.getElementById('container-categorias')
+        const imagens_categorias = document.createElement('img')
+        imagens_categorias.src = imgCatLocal
+        container_categorias.appendChild(imagens_categorias)    
+    }
+
+
+async function mostrarCategorias(){
+    const imagens_categorias_final = await pegarCategorias()
+    imagens_categorias_final.forEach(criarCategorias)
+}
+
+
+//area de teste
+mostrarCategorias()
+
+
